8ba35852ad6113fe18f641ef1ca2b2d8d09c1f68
manny405 <imanuel.manny.bier@gmail.com>
2022-08-26 14:23:05 -0400

    setting versions

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-      name='sapai',
-      version='0.1.0',
-      packages=['sapai',
-                ],
-      #find_packages(exclude=[]),
-      install_requires=['numpy', 'keras','torch', 'graphviz'],
-      data_files=[],
-      )
+    name="sapai",
+    version="0.1.0",
+    packages=[
+        "sapai",
+    ],
+    # find_packages(exclude=[]),
+    install_requires=["numpy", "keras", "torch", "graphviz"],
+    data_files=[],
+)
